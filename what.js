f48eff132907996e5953158821591b95a77cb80d
IntroToScriptingLanguages <zahntastic@gmail.com>
2015-09-30 18:02:58 -0500

    innerhtml isn't changing text...

--- what.js
+++ what.js
@@ -1,14 +1,22 @@
 var essay = document.getElementById('essay');
 var textarea = document.getElementById('what');
-var
+var stop = document.getElementById('stop');
 var instruct = document.getElementById('instruct');
 var form = document.getElementsByTagName('form')[0];
+var pause = false;
+var input = "";
 
 textarea.addEventListener('change', function() {
    form.submit();
+   input = getInput();
+   essay.innerHTML += "input";
 }, false);
+essay.innerHTML += "input";
 
-var what = location.search;
-what = what.replace('?what=', '');
-what = what.replace(/\+/g, ' ');
-what = decodeURIComponent(what);
+function getInput() { //Returns input from the event
+  var what = location.search;
+  what = what.replace('?what=', '');
+  what = what.replace(/\+/g, ' ');
+  what = decodeURIComponent(what);
+  return what;
+}
