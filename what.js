5d42f1890eac1312cd630d7384bfb8930718fb59
IntroToScriptingLanguages <zahntastic@gmail.com>
2015-09-30 18:28:12 -0500

    bug fixed!

--- what.js
+++ what.js
@@ -1,22 +1,20 @@
 var essay = document.getElementById('essay');
 var textarea = document.getElementById('what');
+var text = "";
 var stop = document.getElementById('stop');
 var instruct = document.getElementById('instruct');
 var form = document.getElementsByTagName('form')[0];
 var pause = false;
-var input = "";
 
 textarea.addEventListener('change', function() {
-   form.submit();
-   input = getInput();
-   essay.innerHTML += "input";
+   text = getText();
+   essay.innerHTML = essay.innerHTML + text;
+   console.log(text);
 }, false);
-essay.innerHTML += "input";
 
-function getInput() { //Returns input from the event
-  var what = location.search;
-  what = what.replace('?what=', '');
-  what = what.replace(/\+/g, ' ');
-  what = decodeURIComponent(what);
-  return what;
+
+function getText() { //Gets the text from the textarea
+  var text_to_get = textarea.value;
+  textarea.value = "";
+  return text_to_get;
 }
